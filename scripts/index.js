8b8440346c7771287a3bd70ae201404427afb9cf
Семён Семёнов <semyon_semyonov_2003@bk.ru>
2024-04-18 22:14:21 +0400

    review1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const placesList = document.querySelector(".places__list"); // заводим переменную
 
-// @todo: DOM узлы
+function AddCard(element, deleteCard) { // функция по добавлению карточки и поиска через DOM элементов
 
-// @todo: Функция создания карточки
+  const card_template = document.querySelector("#card-template").content; // находим карточку temlate
+  const card_element = card_template.cloneNode(true); //содержит ли карточка элемент
+  const title = card_element.querySelector(".card__title"); // находим описание
+  const img = card_element.querySelector(".card__image"); // находим картинку
 
-// @todo: Функция удаления карточки
+  title.textContent = element.name;
 
-// @todo: Вывести карточки на страницу
+  img.src = element.link;
+  img.alt = element.name;
+
+  const delete_button = card_element.querySelector(".card__delete-button");
+  
+  delete_button.addEventListener("click", deleteCard);
+  return card_element;
+}
+
+function deleteCard(event) { // фуникция по удалению карточки 
+  const card = event.target.closest(".card");
+  card.remove();
+}
+
+initialCards.forEach((element) => {
+  placesList.append(AddCard(element, deleteCard));
+});
