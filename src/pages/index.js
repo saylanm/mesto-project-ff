208e79cabb12585adb5af4658810ab9a9404568e
Семён Семёнов <semyon_semyonov_2003@bk.ru>
2024-06-05 13:45:06 +0400

    new_api_and_indexjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,42 +4,44 @@ import { openPopup, closePopup } from '../scripts/modal.js';
 import { enableValidation, clearValidation } from '../scripts/validation.js';
 import { getUserData, getInitialCards, patchUserInfo, postNewCard, deleteCard, likeCard, deleteLike, editAvatar } from '../scripts/api.js';
 import { config } from '../scripts/validationConfig.js';
-
+// находим эелементы DOM
 const formEditProfile = document.forms['edit-profile'];
 const formNewPlace = document.forms['new-place'];
 const popups = document.querySelectorAll('.popup');
 const cardList = document.querySelector('.places__list');
-const editButton = document.querySelector('.profile__edit-button');
+const profileEditButton = document.querySelector('.profile__edit-button');
 const popupTypeEdit = document.querySelector('.popup_type_edit');
 const profileAddButton = document.querySelector('.profile__add-button');
 const popupTypeNewCard = document.querySelector('.popup_type_new-card');
-const editProfileName = formEditProfile.elements.name;
-const editProfileDescription = formEditProfile.elements.description;
+const inputProfileName = formEditProfile.elements.name;
+const inputProfileDescription = formEditProfile.elements.description;
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 const imagePopup = document.querySelector('.popup_type_image');
 const image = document.querySelector('.popup__image');
 const caption = document.querySelector('.popup__caption');
 const forms = document.querySelectorAll('.popup__form');
-const formEditProfileInputs = Array.from(formEditProfile.querySelectorAll('.popup__input'));
-const formEditProfileButton = formEditProfile.querySelector('.popup__button');
-const formNewPlaceInputs = Array.from(formNewPlace.querySelectorAll('.popup__input'));
-const formNewPlaceButton = formNewPlace.querySelector('.popup__button');
+const inputsFormEditProfile = Array.from(formEditProfile.querySelectorAll('.popup__input'));
+const buttonFormEditProfile = formEditProfile.querySelector('.popup__button');
+const inputsFormNewPlace = Array.from(formNewPlace.querySelectorAll('.popup__input'));
+const buttonFormNewPlace = formNewPlace.querySelector('.popup__button');
 const profileImage = document.querySelector('.profile__image');
-const formNewPlaceInputName = formNewPlace.elements['place-name'];
-const formNewPlaceInputLink = formNewPlace.elements.link;
+const inputNameFormNewPlace = formNewPlace.elements['place-name'];
+const inputLinkFormNewPlace = formNewPlace.elements.link;
 const popupTypeQuestion = document.querySelector('.popup_type_question');
 const popupTypeQuestionButton = document.querySelector('.popup_type_question .button');
 const popupTypeNewAvatar = document.querySelector('.popup_type_new-avatar');
 const avatar = document.querySelector('.profile__image');
 const formEditAvatar = document.forms['edit-avatar'];
-const formEditAvatarInput = formEditAvatar.elements.link;
-const formEditAvatarButton = formEditAvatar.querySelector('.popup__button')
+const inputFormEditAvatar = formEditAvatar.elements.link;
+const buttonFormEditAvatar = formEditAvatar.querySelector('.popup__button');
+let card;
+let cardId;
 
 // Добавить карточки
 Promise.all([getUserData(), getInitialCards()])
-  .then((res) => {
-    const [userData, cardsData] = res;
+.then((res) => {
+  const [userData, cardsData] = res;
       cardsData.forEach((item) => {
         renderCard(item, userData._id, "append");
       })
@@ -54,29 +56,10 @@ function setProfileInfo (userData) {
 }
 
 function renderCard(item, userId, method = "prepend") {
-  const cardElement = createCard(item.name, item.link, handleImageClick);
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-  const likeButton = cardElement.querySelector('.card__like-button');
-
-  if(!(item.owner._id === userId)) {
-    deleteButton.setAttribute('style', 'display: none');
-  }
-
-  if(item.likes.length > 0) {
-    likeButton.dataset.after = item.likes.length;
-  }
-
-  if(isLiked(item, userId)) {
-    likeButton.classList.add('card__like-button_is-active')
-  }
-
-  deleteButton.addEventListener('click', (evt) => handleDeleteButtonClick(item, userId, evt));
-  likeButton.addEventListener('click', (evt) => handleLikeButton(item._id, evt, likeButton))
-
+  const cardElement = createCard(item, userId, item.name, item.link, handleImageClick, handleDeleteButtonClick, handleLikeButton);
   cardList[ method ](cardElement);
 }
 
-// Обработчики клика для карточки
 
 function handleImageClick (evt) {
   if (evt.target.classList.contains('card__image')) {
@@ -105,44 +88,35 @@ function handleLikeButton (id, evt, likeButton) {
   }
 }
 
-function isLiked (item, userId) {
- return item.likes.some((element) => {
-  return element._id === userId
- })
-}
-
-function handleDeleteButtonClick (item, userId, evt) {
+function handleDeleteButtonClick (item, evt) {
   openPopup(popupTypeQuestion);
-  popupTypeQuestionButton.addEventListener('click', () => handleYesButtonClick(item, userId, evt))
+  card = evt.target.closest('.card');
+  cardId = item._id;
 }
 
-// я не знал, что это доп задание... 
-
-function handleYesButtonClick (item, userId, evt) {
-  if((item.owner._id === userId)) {
-    deleteCard(item._id)
-      .then(() => {
-        const card = evt.target.closest('.card');
-        card.remove();
-        popupTypeQuestionButton.removeEventListener('click', () => handleYesButtonClick(item, userId, evt));
-        closePopup(popupTypeQuestion);
-      })
-      .catch((err) => console.log(err))
-  }
+function handleYesButtonClick () {
+  deleteCard(cardId)
+  .then(() => {
+    card.remove();
+    closePopup(popupTypeQuestion);
+  })
+  .catch((err) => console.log(err))
 }
+
+popupTypeQuestionButton.addEventListener('click', handleYesButtonClick);
 
 // Открыть Форму "редактировать профиль"
 
-editButton.addEventListener('click', () => {
+profileEditButton.addEventListener('click', () => {
   openPopup(popupTypeEdit);
-  clearValidation(config, formEditProfileInputs, formEditProfileButton, formEditProfile);
-  editProfileName.value = profileTitle.textContent;
-  editProfileDescription.value = profileDescription.textContent;
+  clearValidation(config, inputsFormEditProfile, buttonFormEditProfile, formEditProfile);
+  inputProfileName.value = profileTitle.textContent;
+  inputProfileDescription.value = profileDescription.textContent;
   // clearValidation очищает поля и переключает кнопку. После установки значений в поля
   // Для clearValidation они не видны и кнопка остается disabled=true. Есди поменять места
   // Очистятся инпуты. Далее костыль
-  formEditProfileButton.disabled = false;
-  formEditProfileButton.classList.remove('submit_inactive');
+  buttonFormEditProfile.disabled = false;
+  buttonFormEditProfile.classList.remove('submit_inactive');
 });
 
 // Сохранить изменения формы "редактировать профиль"
@@ -151,78 +125,80 @@ formEditProfile.addEventListener('submit', (evt) => handleFormEditProfile(evt));
 
 // UX "сохранить"
 
-function handleSaveButton (evt, button) {
+function handleSaveButton (evt, button, buttonText = "Сохранение...") {
   evt.preventDefault();
-  button.innerHTML = 'Сохранение<span>.</span><span>.</span><span>.</span>'
-  const dots = button.querySelectorAll('span');
-  dots.forEach((item) => {
-    item.style.display = 'inline-block';
-  })
+  button.textContent = [ buttonText ];
 }
 
 function handleFormEditProfile (evt) {
-  handleSaveButton(evt, formEditProfileButton);
-  patchUserInfo(editProfileName, editProfileDescription)
+  handleSaveButton(evt, buttonFormEditProfile);
+  patchUserInfo(inputProfileName, inputProfileDescription)
   .then((data) => {
     profileTitle.textContent = data.name;
     profileDescription.textContent = data.about;
-    formEditProfileButton.textContent = 'Сохранить';
     closePopup(popupTypeEdit);
   })
   .catch((err) => console.log(err))
+  .finally(() => {
+    buttonFormEditProfile.textContent = 'Сохранить';
+  })
 }
 
-// Открыть Форму "добавить карточку"
+// Открывае Форму "добавление карточки"
 
 profileAddButton.addEventListener('click', () => {
   openPopup(popupTypeNewCard);
-  clearValidation(config, formNewPlaceInputs, formNewPlaceButton, formNewPlace);
+  clearValidation(config, inputsFormNewPlace, buttonFormNewPlace, formNewPlace);
 });
 
-// Добавить новую карточку
+// Добавляем новую карточку
 
 formNewPlace.addEventListener('submit', (evt) => {
   handleFormNewPlace(evt);
 });
 
 function handleFormNewPlace (evt) {
-  handleSaveButton(evt, formNewPlaceButton);
-  postNewCard(formNewPlaceInputName, formNewPlaceInputLink)
+  handleSaveButton(evt, buttonFormNewPlace);
+  postNewCard(inputNameFormNewPlace, inputLinkFormNewPlace)
   .then((data) => {
     renderCard(data, data.owner._id)
-    formNewPlaceButton.textContent = 'Сохранить';
     closePopup(popupTypeNewCard);
   })
   .catch((err) => console.log(err))
+  .finally(() => {
+    buttonFormNewPlace.textContent = 'Сохранить';
+  })
 }
 
-// Изменить аватар
+// Изменяем аватар
 
 avatar.addEventListener('click', handleAvatarClick);
 
 function handleAvatarClick () {
   openPopup(popupTypeNewAvatar);
-  clearValidation(config, [formEditAvatarInput], formEditAvatarButton, formEditAvatar);
+  clearValidation(config, [inputFormEditAvatar], buttonFormEditAvatar, formEditAvatar);
 }
 
 formEditAvatar.addEventListener('submit', handleFormEditAvatar)
 
 function handleFormEditAvatar (evt) {
-  handleSaveButton(evt, formEditAvatarButton);
-  editAvatar(formEditAvatarInput)
+  handleSaveButton(evt, buttonFormEditAvatar);
+  editAvatar(inputFormEditAvatar)
   .then((data) => {
     avatar.src = data.avatar;
-    formEditAvatarButton.textContent = 'Сохранить';
     closePopup(popupTypeNewAvatar);
   })
   .catch((err) => console.log(err))
+  .finally(() => {
+    buttonFormEditAvatar.textContent = 'Сохранить';
+  })
 }
 
-// Включить валидацию
+// Включаем валидацию
 
 enableValidation(config, forms);
 
-// Закрыть попапы
+// Закрываем попапы
 
 popups.forEach((item) => {
   item.addEventListener('mousedown', (evt) => {
@@ -235,6 +211,3 @@ popups.forEach((item) => {
     }
   })
 })
-
-// Токен: 2e1b0b74-43cb-43a1-b169-23f9e934c10d
-// Идентификатор группы: wff-cohort-14
--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,15 +1,31 @@
 const cardTemplate = document.querySelector('#card-template');
 
-function createCard(name, link, handleImageClick) {
+function createCard(item, userId, name, link, handleImageClick, handleDeleteButtonClick, handleLikeButton) {
   const listItemCopy = cardTemplate.content.cloneNode(true);
   const cardImage = listItemCopy.querySelector('.card__image');
   const cardTitle = listItemCopy.querySelector('.card__title');
-  
+  const deleteButton = listItemCopy.querySelector('.card__delete-button')
+  const likeButton = listItemCopy.querySelector('.card__like-button');
+
   cardTitle.textContent = name;
   cardImage.src = link;
   cardImage.alt = name;
 
+  if(!(item.owner._id === userId)) {
+    deleteButton.setAttribute('style', 'display: none');
+  }
+
+  if(item.likes.length > 0) {
+    likeButton.dataset.after = item.likes.length;
+  }
+
+  if(isLiked(item, userId)) {
+    likeButton.classList.add('card__like-button_is-active')
+  }
+
   cardImage.addEventListener('click', handleImageClick);
+  deleteButton.addEventListener('click', (evt) => handleDeleteButtonClick(item, evt));
+  likeButton.addEventListener('click', (evt) => handleLikeButton(item._id, evt, likeButton));
 
   return listItemCopy
 }
@@ -17,5 +33,11 @@ function createCard(name, link, handleImageClick) {
 function like (evt) {
   evt.target.classList.toggle('card__like-button_is-active');
 }
+
+function isLiked (item, userId) {
+  return item.likes.some((element) => {
+   return element._id === userId
+  })
+ }
 
 export {createCard, like}
